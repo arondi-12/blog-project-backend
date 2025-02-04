fd6d4d0595374fa69d0cc0b606fbc58b87a1b81d
arondi-12 <davieariwi@gmail.com>
2025-02-04 20:51:09 +0300

    Render changes

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
 from app import create_app
+import os
 
 # Create the app using the create_app function from __init__.py
 app = create_app()
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    print(f"Starting app on 0.0.0.0:{port}")
+    app.run(host='0.0.0.0',port=port, debug=True)
